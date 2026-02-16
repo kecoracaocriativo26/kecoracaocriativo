0208faf82e536aa75adb050396bab520eaebc5d1
kecoracaocriativo26 <kecoracaocriativo26@gmail.com>
2026-02-16 14:53:59 -0300

    Add files via upload
    DONE DEPLOY

---
+++ script.js
@@ -0,0 +1,14 @@
+const CONFIG = {
+    whatsappNumber: "554991439222",
+    instagramUser: "https://www.instagram.com/detalhesqueencantamke?igsh=MXY5ZXp3OWoyam9jaw%3D%3D&utm_source=qr", 
+    whatsappMessage: "Olá! Gostaria de fazer um orçamento!"
+};
+
+function abrirWhatsApp() {
+    window.open(`https://wa.me/${CONFIG.whatsappNumber}?text=${encodeURIComponent(CONFIG.whatsappMessage)}`, '_blank');
+}
+
+function abrirInstagram() { 
+    window.open(CONFIG.instagramUser, '_blank'); 
+}
+
